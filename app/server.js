e0bb2bb31cf807af7a244764f98f724973537dc1
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2023-11-23 12:33:52 +0300

    boilerplate code

---
+++ app/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import Memcached from 'memcached';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+// const memcached = new Memcached(`${process.env.MADDR}:${process.env.MPORT}`);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', async (req, res) => {
+	res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server started'));
