989f84882cd26c7bdc22b085da7b2f2f082a86c6
Mikhail <mixa.penkin@gmail.com>
2023-12-08 21:56:30 +0300

    added nodemon and a convenient link

--- app/server.js
+++ app/server.js
@@ -1,28 +1,28 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dbConnect } from './db/memcached.js';
-import indexRouter from './routes/index.js';
-import authRouter from './routes/auth.js';
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dbConnect } from './db/memcached.js'
+import indexRouter from './routes/index.js'
+import authRouter from './routes/auth.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const db = dbConnect(`${process.env.MADDR}:${process.env.MPORT}`);
+const app = express()
+const db = dbConnect(`${process.env.MADDR}:${process.env.MPORT}`)
 
-app.use(cookieParser());
-app.use(express.json());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.get('*', (req, res) => res.status(404).end('Page not found'));
+app.use(cookieParser())
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/', indexRouter)
+app.use('/auth', authRouter)
+app.get('*', (req, res) => res.status(404).end('Page not found'))
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
